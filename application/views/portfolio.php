771a2e3b886fa0b3d08b4172c45d998b30da2cdb
roffs <rofsdg@gmail.com>
2015-05-01 12:48:47 -0500

    criacao de index

---
+++ application/views/portfolio.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Jason Canto</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Bem vindo!</h1>
+
+	<div id="body">
+		<p>Este eh meu portfolio em php</p>
+		<code>Divirta-se</code>
+	</div>
+</div>
+
+</body>
+</html>
