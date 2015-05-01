d1299d3b95f6bedc3b0b7fb8d257698d1598916e
roffs <rofsdg@gmail.com>
2015-05-01 18:23:34 -0500

    teste

--- application/controllers/Portfolio.php
+++ application/controllers/Portfolio.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Lalala extends CI_Controller {
+class Portfolio extends CI_Controller {
 
 	public function index()
 	{
