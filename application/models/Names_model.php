07cb0303267471b6b43a62f97c8078e3beaf1ffc
roffs <rofsdg@gmail.com>
2015-05-06 21:49:22 -0500

    query

--- application/models/Names_model.php
+++ application/models/Names_model.php
@@ -4,7 +4,7 @@ class Names_model extends CI_Model{
 
 	public function getNames(){
 		try{
-			$query = $this->db->query('Select * from names limit 10');	
+			$query = $this->db->query('Select id, name from names');	
 		}catch(Exeception $e){
 			log_message( 'error', $e->getMessage( ) . ' in ' . $e->getFile() . ':' . $e->getLine() );
 		}
