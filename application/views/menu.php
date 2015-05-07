dba922c624b8f4b7d517f7ca9ca726924b7cda3c
roffs <rofsdg@gmail.com>
2015-05-06 21:58:36 -0500

    names

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,6 +1,7 @@
 <div id="main_menu"> 
 	<ul>
 		<li><a href="<?php echo base_url();?>">Home</a></li>
-		<li><a href="<?php echo base_url();?>index.php/cv">cv</a></li>
+		<li><a href="<?php echo base_url();?>/cv">cv</a></li>
+		<li><a href="<?php echo base_url();?>/names">names</a></li>
 	</ul>
 </div>
--- application/views/names.php
+++ application/views/names.php
@@ -4,30 +4,37 @@
 <head>
 	<title>Busca por nome</title>
 	<script type="text/javascript" src="<?php base_url() ?>content/js/jquery-1.11.2.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('content/css/style.css'); ?>">
 </head>
 <body>
-	<p>Busca por nome:</p>
-	<input type="text" id="busca"/>
-	<hr>
-	<script type="text/javascript">
-	$(document).ready(function(){
-		var json = <?php echo $json?>;
-		$('#busca').keyup(function(){
-			$('#resultado').empty();
-			var str = $(this).val().toLowerCase();
-			if(str!='' && str!=null)
-				busca(str);
-		});
-		function busca(str){
-			$.each(json,function(){
-				if(this.name.toLowerCase().indexOf(str)>=0){
-					$('#resultado').append('<li>'+this.name+'</li>');
+	<div id="page">
+		<article id="content">
+			<?php $this->load->view('menu');?>
+			<p>Busca por nome:</p>
+			<input type="text" id="busca"/>
+			<hr>
+			<script type="text/javascript">
+			$(document).ready(function(){
+				var json = <?php echo $json?>;
+				$('#busca').keyup(function(){
+					$('#resultado').empty();
+					var str = $(this).val().toLowerCase();
+					if(str!='' && str!=null)
+						busca(str);
+				});
+				function busca(str){
+					$.each(json,function(){
+						if(this.name.toLowerCase().indexOf(str)>=0){
+							$('#resultado').append('<li>'+this.name+'</li>');
+						}
+					});			
 				}
-			});			
-		}
 
-	});
-	</script>
-	<ul id="resultado"></ul>
+			});
+			</script>
+			<ul id="resultado"></ul>
+		</article>
+	</div>
+
 </body>
 </html>
