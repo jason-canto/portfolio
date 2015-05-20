3e912b7cb181a5edee308154b33de7a15d2e7739
roffs <rofsdg@gmail.com>
2015-05-20 17:20:58 -0500

    Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,7 +6,7 @@ class Login extends CI_Controller{
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
-		$this->load->library('session');
+		$this->load->driver('session');
 	}
 	public function teste(){
 		$user = array('name'=>'jason','password'=>'lala');
