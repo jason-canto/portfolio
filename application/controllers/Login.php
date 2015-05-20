8cffc21e784aee073d8a9b23f03824592ff1f5ba
roffs <rofsdg@gmail.com>
2015-05-20 16:58:41 -0500

    Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,10 +5,11 @@ class Login extends CI_Controller{
 
 	public function __contruct(){
 		parent:: __construct();
-		$this->load->libriry('session');
+		$this->load->library('session');
 	}
 	public function teste(){
 		$user = array('name'=>'jason','password'=>'lala');
+
 		$this->session->set_userdata($user);
 		echo $this->session->userdata('user');
 	}
