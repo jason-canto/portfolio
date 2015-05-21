f750062c28e505754f55fed3b47a98949e99cf12
roffs <rofsdg@gmail.com>
2015-05-20 19:18:40 -0500

    htaccess

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -11,7 +11,9 @@ class Login extends CI_Controller{
 	public function teste(){
 		$user = array('name'=>'jason','password'=>'lala');
 
-		$this->session->set_userdata($user);
-		echo $this->session->userdata('user');
+		//$this->session->set_userdata($user);
+		$this->session->set_userdata('some_name', 'some_value');
+		print_r($this->session->userdata('some_name'));
+		echo 'lala';
 	}
 }
